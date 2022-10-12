a810e40b7f334ca9ca1f185c86ce929abd66e3d2
Alya_Abdulla <3lya3bdulla@gmail.com>
2022-10-12 14:27:51 +0300

    solve 7Question

--- solutions.sql
+++ solutions.sql
@@ -1,21 +1,59 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+
+            Select subject
+            from subjects
+            order by subject;
 -- 2. Find all subjects sorted by location
 
--- ### Where
+            from subjects
+            order by location ;
+
+-- ### WhereSelect subject
 -- 3. Find the book "Little Women"
+            SELECT *
+            from books
+            where title='Little Women';
+
 -- 4. Find all books containing the word "Python"
+
+            SELECT *
+            from books
+            where title  LIKE '%Python%';
+
 -- 5. Find all subjects with the location "Main St" sort them by subject
+            SELECT *
+            from subjects
+            where location='Main St'
+            order by subject
+
 
 
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+            Select *
+            from books b
+            inner join subjects s
+            on b.subject_id=s.id
+            where s.subject like'%Computers%'
+
+
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
 -- 	* Author's last name
 -- 	* Book subject
+
+
+            SELECT b.title ,a.first_name, a.last_name ,s.subject
+            from books b
+            join authors a on b.author_id=a.id
+            join subjects s on b.subject_id =s.id
+
+
+
+
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
 -- 	* Display ONLY: title and price
